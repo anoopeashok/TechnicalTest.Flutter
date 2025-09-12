c67bd3b6e8e5dc3a79ba423fc2fd3ae72e46b6e2
Anoop Ashok <anoop.e.ashok@gmail.com>
2025-09-12 13:47:19 +0100

    added retry

--- lib/utils/error_widgets.dart
+++ lib/utils/error_widgets.dart
@@ -1,22 +1,33 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_tech_task/l10n/app_localizations.dart';
 import 'package:flutter_tech_task/utils/app_exceptions.dart';
 
 class ErrorMessage extends StatelessWidget {
-  String message;
-  ErrorMessage({super.key,this.message = "Something went wrong"});
+  String? message;
+  Function? callback;
+  ErrorMessage({super.key,this.message, this.callback});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.center,
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(Icons.error, color: Colors.red, size: 50),
           SizedBox(height: 10),
-          Text(message, style: TextStyle(fontSize: 18)),
+          Text(message ?? AppLocalizations.of(context)!.defaultErrorMessage, style: TextStyle(fontSize: 18)),
+          SizedBox(height: 10),
+          callback != null ? TextButton(
+            onPressed: () {
+              callback!();
+            },
+            child: Text('Retry'),
+          ) : SizedBox.shrink(),
         ],
       ),
     );
